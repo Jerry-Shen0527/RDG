82458a812ac3992e8211432addcdf16eb7fb41ae
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2023-05-01 00:20:47 +0800

    Update

--- include/RDG/CUDA/CUDAExternal.h
+++ include/RDG/CUDA/CUDAExternal.h
@@ -10,4 +10,6 @@ bool importBufferToBuffer(
 CUdeviceptr mapBufferToCUDABuffer(nvrhi::IBuffer* pBuf, uint32_t cudaUsageFlags, nvrhi::IDevice* device);
 CUtexObject
 mapTextureToCudaTex(nvrhi::ITexture* image_handle, uint32_t cudaUsageFlags, nvrhi::IDevice* device);
+CUsurfObject
+mapTextureToSurface(nvrhi::ITexture* image_handle, uint32_t cudaUsageFlags, nvrhi::IDevice* device);
 #endif
