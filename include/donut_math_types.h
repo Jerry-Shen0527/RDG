e2ca40e0a7ad0e06aadc8a0448f9429d130869a2
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2023-05-30 21:01:54 +0800

    Update

--- include/donut_math_types.h
+++ include/donut_math_types.h
@@ -6,6 +6,7 @@ using uint = unsigned;
 using float3x4 = glm::mat3x4;
 using float4x4 = glm::mat4x4;
 #else
+#include "donut/core/math/math.h"
 using donut::math::float3x4;
 using donut::math::float4x4;
 #endif
